d17c64283fd004d1014b85ff9bccc648f621a14a
Lucas Teixeira <lucasteixeira8784@gmail.com>
2024-07-16 18:47:57 -0300

    First commit

---
+++ src/test/java/br/com/alura/ForumHUB/ForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.ForumHUB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
